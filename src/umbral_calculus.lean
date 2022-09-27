6eef61696a4f6b57e99f9a58912a2311100842e8
mikesha2 <32970136+mikesha2@users.noreply.github.com>
2022-09-27 15:58:59 -0400

    Update umbral_calculus.lean

--- src/umbral_calculus.lean
+++ src/umbral_calculus.lean
@@ -66,19 +66,20 @@ namespace umbral_calculus
     end,
   }
 
-  def shift_linear_map (a : R) : linear_map (shift_ring_hom a) R[X] R[X] :=
+  def shift_linear_map (a : R) : linear_map (shift_ring_hom a : R[X] →+* R[X]) R[X] R[X] :=
   {
-    to_fun := shift_a_operator a,
+    to_fun := shift_ring_hom a,
     map_add' := begin
       intros x y,
       repeat {rw shift_a_operator},
-      rw polynomial.add_comp,
+      have := (shift_ring_hom a).map_add' x y,
+      simp,
     end,
     map_smul' := begin
       intros x y,
       repeat {rw shift_a_operator},
+      have := (shift_ring_hom a).map_mul' x y,
       simp,
-      refl,
     end,
   }
 
@@ -97,34 +98,38 @@ namespace umbral_calculus
     end
   }
 
-  class shift_invariant_op {a : R} (b : R) (op : R[X] →ₗ[R] R[X]) :=
-    (is_shift_invariant : ∀ (p : R[X]), op (shift_linear_map b p) = (shift_linear_map b) (op p))
-/-
+  class shift_invariant_op (op : R[X] →+* R[X]) :=
+    (is_shift_invariant : ∀ (a : R), ring_hom.comp 
+      op (shift_linear_map a) = (shift_linear_map a).to_fun (op.to_fun p))
+
   -- The shift operators are shift invariant
-  instance shift_a_operator_is_shift_invariant_op {a : R} : 
-    shift_invariant_op (shift_linear_map a) :=
+  instance shift_a_operator_is_shift_invariant_op (a : R) : 
+    shift_invariant_op (shift_ring_hom a) :=
   {
     is_shift_invariant := begin
-      intros k p,
+      intros p q,
       repeat {rw shift_a_operator},
-      rw polynomial.comp_assoc,
+      simp,
+      ext k,
+      rintro ⟨g, hg, rfl⟩,
     end,
   }
-  
-  class is_delta_op (op : R[X] → R[X]) extends is_shift_invariant_op op :=
+
+  class delta_op (a : R) (op : R[X] →+* R[X]) extends shift_invariant_op a op :=
     (is_delta : ∃ (c : R), op polynomial.X = polynomial.C c ∧ c ≠ 0)
-  
+
   /- This lemma specifically requires a ring structure on R 
      so that we can use the additive group property add_right_inj.
      The previous theorems only require a semiring.
   -/
-  lemma delta_constant_is_zero (op : R[X] → R[X]) [is_delta_op op] :
+  lemma delta_constant_is_zero (op : R[X] →+* R[X]) [delta_op a op] :
     ∀ (a : R), op (polynomial.C a) = 0 :=
   begin
     intro a,
-    have q₁ := _inst_2.is_shift_invariant a polynomial.X,
+    have q₁ := _inst_2.is_shift_invariant polynomial.X,
     have q₂ := _inst_2.is_delta,
     cases q₂ with c q₃,
+    ext q₁,
     rw q₃.1 at q₁,
     rw [shift_a_operator, polynomial.X_comp,
       _inst_2.map_add polynomial.X (polynomial.C a), q₃.1,
@@ -133,7 +138,7 @@ namespace umbral_calculus
     rw add_right_inj at q₁,
     exact q₁,
   end
-
+/-
   variables (delta_op : R[X] → R[X]) [is_delta_op delta_op]
 
   lemma delta_decreases_degree (p : R[X]) : (delta_op (polynomial.X ^ (n + 1))).degree = n :=
